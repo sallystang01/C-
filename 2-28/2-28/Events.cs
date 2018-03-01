77b6f0dfaa2f00a9e3ef898ec38331a989c56462
sallystang01 <hunterduerr@gmail.com>
2018-02-28 21:22:44 -0500

    Publish

---
+++ 2-28/2-28/Events.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_28
+{
+    class Events
+    {
+        //Variables
+        private int EventID;
+        public string EventTitle;
+        public DateTime EventDate;
+        private int StaffID;
+        public TimeSpan StartTime;
+        public TimeSpan EndTime;
+        public int SeriesFlag;
+
+        
+        //First Overload
+        public Events()
+
+        {
+            EventID = 5;
+            EventTitle = "Why are Programmers So Smart?";
+            EventDate = DateTime.Parse("2017-01-05");
+            StaffID = 5;
+            StartTime = new TimeSpan(12, 00, 00);
+            EndTime = new TimeSpan(2, 30, 00);
+            SeriesFlag = 0;
+            
+;            
+        }
+
+        
+        //Second Overload
+        public Events(int id, string en, DateTime ed, int sid, TimeSpan st, TimeSpan et,
+                int sf)
+
+        {
+            EventID = id;
+            EventTitle = en;
+            ed = EventDate;
+            StaffID = sid;
+            StartTime = st;
+            EndTime = et;
+            SeriesFlag = sf;
+        }
+        //Message Displayed
+        public void EventMessage()
+        {
+            Console.WriteLine(@"Welcome to Event #{0}: 
+    '{1}' 
+    hosted on {2}
+    by Staff Member ID#{3}.
+    This event will start at {4} and end at {5}.
+    Series Indicator: {6}", EventID, EventTitle, EventDate.ToString("dd/MM/yyyy"), StaffID, StartTime, EndTime, SeriesFlag);
+
+        }
+    }
+}
---
+++ 2-28/2-28/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Events e1 = new Events();
+            e1.EventMessage();
+
+
+
+            Events e2 = new Events(1, "The History of Human Emotions", DateTime.Parse("2017-01-12"), 1,
+                        TimeSpan.Parse("12:00:00"), TimeSpan.Parse("02:00:00"), 0);
+            e2.EventMessage();
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
