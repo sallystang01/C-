cf6b7313eb5b5aeb7c1698df49505bf1d4e64878
sallystang01 <hunterduerr@gmail.com>
2018-02-28 21:43:29 -0500

    Done

--- 2-28/2-28/Members.cs
+++ 2-28/2-28/Members.cs
@@ -8,5 +8,18 @@ namespace _2_28
 {
     class Members
     {
+       private int MemberID;
+        public int RenewalID;
+        public string MemberNumber;
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        public string Email;
+        public string Phone;
+        public string Gender;
+        public string StartDate;
+        public string CurrentFlag;
+        public DateTime Birtdate;
+
     }
 }
